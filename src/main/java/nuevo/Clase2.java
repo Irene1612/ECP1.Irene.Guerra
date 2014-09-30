02c9a209d396c626de20227c605d56e3abdf5f54
Irene1612 <vicmatil.igf@gmail.com>
2014-09-30 19:52:31 +0200

    CR 1.05

--- src/main/java/nuevo/Clase2.java
+++ src/main/java/nuevo/Clase2.java
@@ -2,4 +2,12 @@ package nuevo;
 
 public class Clase2 {
 	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 }
