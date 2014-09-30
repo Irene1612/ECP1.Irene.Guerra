180c106667a874a2b12d9170c532f3dfeda2bc09
Irene1612 <vicmatil.igf@gmail.com>
2014-09-30 19:47:44 +0200

    CD 6

---
+++ src/main/java/nuevo/Clase1.java
@@ -0,0 +1,26 @@
+package nuevo;
+
+public class Clase1 {
+	private int atributo1;
+	private int atributo2;
+
+	public int getAtributo1() {
+		return atributo1;
+	}
+
+	public void setAtributo1(int atributo1) {
+		this.atributo1 = atributo1;
+	}
+
+	public int getAtributo2() {
+		return atributo2;
+	}
+
+	public void setAtributo2(int atributo2) {
+		this.atributo2 = atributo2;
+	}
+	
+	public int doble(){
+		return this.atributo1*2;
+	}
+}
